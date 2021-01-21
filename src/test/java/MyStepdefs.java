6e15090db3dd597ec05ea95ba5b61597d76b72a6
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-01-21 16:15:41 +0200

    Update MyStepdefs.java

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -3,6 +3,7 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import static org.junit.Assert.assertEquals;
 
 import static junit.framework.TestCase.assertTrue;
 
